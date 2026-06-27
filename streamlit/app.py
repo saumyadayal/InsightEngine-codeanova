c5775032791027cfb6a989a0eb42494e8ad74e9b
saumyadayal <saumyaadayall@gmail.com>
2026-06-27 22:58:59 +0530

    Create app.py

---
+++ streamlit/app.py
@@ -0,0 +1,25 @@
+import streamlit as st
+
+st.set_page_config(page_title="InsightEngine")
+
+st.title("🛒 InsightEngine")
+
+st.write("""
+## E-Commerce Customer Analytics Dashboard
+
+Current Modules
+
+✅ Data Cleaning
+
+✅ Exploratory Data Analysis
+
+✅ Customer Segmentation
+
+🚧 Churn Prediction
+
+🚧 Recommendation Engine
+
+🚧 Interactive Dashboard
+""")
+
+st.success("Project under development.")
